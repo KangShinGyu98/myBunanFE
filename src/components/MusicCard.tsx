26124e1d85dece32b8edd80a55733a5f4ec7fa3a
Shingyu Kang <rkdtlseb@naver.com>
2023-09-24 14:27:26 +0900

    [feat] card body 클릭시 링크로 이동

--- src/components/MusicCard.tsx
+++ src/components/MusicCard.tsx
@@ -1,21 +1,16 @@
 import { Box, Card, CardBody, Heading, HStack, Image, Tag, Text } from "@chakra-ui/react";
-import React, { useRef, useState } from "react";
-import { Game } from "../hooks/useGames";
-import getCroppedImageUrl from "../services/image-url";
-import CriticScore from "./CriticScore";
-import Emoji from "./Emoji";
-import PlatformIconList from "./PlatformIconList";
+import { useState } from "react";
 import { Music } from "../hooks/useMusics";
 import YouTube from "react-youtube";
 import YoutubeThumnailMaker from "./YoutubeThumnailMaker";
+import { Link } from "react-router-dom";
 
 interface Props {
   music: Music;
 }
-
 const MusicCard = ({ music }: Props) => {
   const [isPlaying, setIsPlaying] = useState(false);
-  const playerRef = useRef(null);
+  // const playerRef = useRef(null);
   const opts = {
     height: "300",
     width: "400",
@@ -23,10 +18,10 @@ const MusicCard = ({ music }: Props) => {
     //   autoplay: 0,
     // },
   };
-  const onReady = (event: any) => {
-    // YouTube 플레이어가 준비되면 playerRef를 업데이트합니다.
-    playerRef.current = event.target;
-  };
+  // const onReady = (event: any) => {
+  //   // YouTube 플레이어가 준비되면 playerRef를 업데이트합니다.
+  //   playerRef.current = event.target;
+  // };
 
   const onMouseEnter = () => {
     setIsPlaying(true); // 마우스를 올리면 자동 재생 시작
@@ -43,30 +38,32 @@ const MusicCard = ({ music }: Props) => {
 
         {/* <YouTube videoId="liJVSwOiiwg" opts={opts} onReady={onReady} /> */}
       </Box>
-      <CardBody>
-        <HStack justifyContent="space-between" marginBottom={3}>
-          <Text>{music.country}</Text>
-          <Text>{music.genre}</Text>
-          {/* //add music likey and tags */}
-        </HStack>
+      <Link to={`/musics/${music.id}`}>
+        <CardBody>
+          <HStack justifyContent="space-between" marginBottom={3}>
+            <Text>{music.country}</Text>
+            <Text>{music.genre}</Text>
+            {/* //add music likey and tags */}
+          </HStack>
 
-        <HStack>
-          {music.tags.map((tag, idx) => (
-            <Tag key={idx}>{tag}</Tag>
-          ))}
-        </HStack>
-        <Text> 가수 : {music.singer}</Text>
-        <Text> 좋아요 : {music.likes}</Text>
-        <Text> 조회수 : {music.views}</Text>
-        <Text> 등록일 : {music.posted.toString()}</Text>
-        <Text> 발매일 : {music.released.toString()}</Text>
-        <Text> 국가 : {music.country} </Text>
+          <HStack>
+            {music.tags.map((tag, idx) => (
+              <Tag key={idx}>{tag}</Tag>
+            ))}
+          </HStack>
+          <Text> 가수 : {music.singer}</Text>
+          <Text> 좋아요 : {music.likes}</Text>
+          <Text> 조회수 : {music.views}</Text>
+          <Text> 등록일 : {music.posted.toString()}</Text>
+          <Text> 발매일 : {music.released.toString()}</Text>
+          <Text> 국가 : {music.country} </Text>
 
-        <Heading fontSize="2xl">
-          {music.title}
-          {/* <Emoji rating={game.rating_top} /> */}
-        </Heading>
-      </CardBody>
+          <Heading fontSize="2xl">
+            {music.title}
+            {/* <Emoji rating={game.rating_top} /> */}
+          </Heading>
+        </CardBody>
+      </Link>
     </Card>
   );
 };
